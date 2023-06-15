dd998b6c25fc72c82e1a623faaed1dcaef9756e1
Tien Phu <phuquangtien1007@gmail.com>
2023-06-15 10:35:35 +0700

    CRUD successfully

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -29,9 +29,63 @@ namespace BulkyBookWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj); //Them du lieu 
-            _db.SaveChanges(); // Luu du lieu da them vao DB
-            return RedirectToAction("Index"); //Tro ve trang Index
+            if (ModelState.IsValid) //Validation form
+            {
+                _db.Categories.Update(obj); //Them du lieu 
+                _db.SaveChanges(); // Luu du lieu da them vao 
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index"); //Tro ve trang Index
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //Thuc hien sua du lieu
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            //Ten khong duoc trung voi Display Order
+            if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order cannot exactly match Name");
+            }
+            if (ModelState.IsValid) //Validation form
+            {
+                _db.Categories.Add(obj); //Them du lieu 
+                _db.SaveChanges(); // Luu du lieu da them vao 
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index"); //Tro ve trang Index
+            }
+            return View();
+        }
+
+        //Thuc hien xoa du lieu
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,8 +8,12 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [StringLength(100)]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public string DisplayOrder { get; set; }
     }
 }
